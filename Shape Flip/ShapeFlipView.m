f7e2a85bae61cd72d8451e68042456d2fc9968e1
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-13 21:02:30 -0400

    Shape View: added ShapeFlipView
    Deleted the initialization stub in ShapeFlipView.m, as
    well as the comments around drawRect in ShapeFlipView.m.
    ShapeFlipView.m is basically empty.

---
+++ Shape Flip/ShapeFlipView.m
@@ -0,0 +1,17 @@
+//
+//  ShapeFlipView.m
+//  Shape Flip
+//
+//  Created by Hamdan Javeed on 2013-05-13.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import "ShapeFlipView.h"
+
+@implementation ShapeFlipView
+
+- (void)drawRect:(CGRect)rect {
+    
+}
+
+@end
