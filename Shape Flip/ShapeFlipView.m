25d626057e0d19123b4c5545af6ed1b3ad186c9e
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-13 22:31:16 -0400

    ShapeView: added a rounded square to draw
    drawRect: in ShapeFlipView.m has been updated to draw a green filled
    rounded square (rounded rect) in the middle of the view. The rounded
    rect has a black stroke of line width 10.0, and a fill of green. The
    rounded square is drawn with UIBezierPath.

--- Shape Flip/ShapeFlipView.m
+++ Shape Flip/ShapeFlipView.m
@@ -12,6 +12,10 @@
 
 #define ROUNDED_RECT_CORNER_RADIUS 20.0
 
+#define SQUARE_WIDTH 100.0
+#define SQUARE_HEIGHT 100.0
+#define SQUARE_LINE_WIDTH 10.0
+
 - (void)drawRect:(CGRect)rect {
     // create the rounded rect and add a clip
     UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:ROUNDED_RECT_CORNER_RADIUS];
@@ -20,6 +24,20 @@
     // set fill color to white and fill
     [[UIColor whiteColor] setFill];
     UIRectFill(self.bounds);
+    
+    // draw a rounded square
+    // create the path
+    CGRect roundedSquareRect = CGRectMake(self.bounds.size.width / 2 - SQUARE_WIDTH / 2, self.bounds.size.height / 2 - SQUARE_HEIGHT / 2, SQUARE_WIDTH, SQUARE_HEIGHT);
+    UIBezierPath *roundedSquarePath = [UIBezierPath bezierPathWithRoundedRect:roundedSquareRect cornerRadius:ROUNDED_RECT_CORNER_RADIUS];
+    
+    // set the stroke and fill
+    [[UIColor blackColor] setStroke];
+    [roundedSquarePath setLineWidth:SQUARE_LINE_WIDTH];
+    [[UIColor greenColor] setFill];
+    
+    // draw the square
+    [roundedSquarePath stroke];
+    [roundedSquarePath fill];
 }
 
 @end
