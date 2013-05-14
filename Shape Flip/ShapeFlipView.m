fbb6c25b7507d5aed4ab5304994f07be7403f368
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-13 21:43:58 -0400

    ShapeView: draw a white rounded rect
    Implement drawRect very basically. All that's drawn is a white rounded
    rect.

--- Shape Flip/ShapeFlipView.m
+++ Shape Flip/ShapeFlipView.m
@@ -10,8 +10,16 @@
 
 @implementation ShapeFlipView
 
+#define ROUNDED_RECT_CORNER_RADIUS 20.0
+
 - (void)drawRect:(CGRect)rect {
+    // create the rounded rect and add a clip
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:ROUNDED_RECT_CORNER_RADIUS];
+    [roundedRect addClip];
     
+    // set fill color to white and fill
+    [[UIColor whiteColor] setFill];
+    UIRectFill(self.bounds);
 }
 
 @end
